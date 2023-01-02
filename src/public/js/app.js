338a10f0733f14c2fb1eda0d4e04efe0bc735518
sugenius77 <zziinn812@gmail.com>
2023-01-02 22:54:10 +0900

    fix: 카메라 변경 시, 상대방 stream에 반영이 안되는 문제 재수정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -87,7 +87,7 @@ function handleCameraClick() {
 async function handleCameraChange() {
   await getMedia(cameraSelect.value);
   if (myPeerConnection) {
-    const videoTrack = myStream.getAudioTracks()[0];
+    const videoTrack = myStream.getVideoTracks()[0];
     const videoSender = myPeerConnection
       .getSenders()
       .find((sender) => sender.track.kind === "video");
